d5c2d4b81c1a8e90a20a7b3013302987173305e4
a23barho <azim.barhoumi@imt-atlantique.net>
2025-02-06 21:58:11 +0700

    Creating ProductController class

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
@@ -0,0 +1,43 @@
+package id.ac.ui.cs.advprog.eshop.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+
+import id.ac.ui.cs.advprog.eshop.model.Product;
+import id.ac.ui.cs.advprog.eshop.service.ProductService;
+
+@Controller
+@RequestMapping("/Product")
+public class ProductController {
+
+	@Autowired
+	private ProductService service;
+	
+	@GetMapping("/create")
+	public String createProductPage(Model model) {
+		Product product = new Product();
+		model.addAttribute("product",product);
+		return "createProduct";
+	}
+	
+	@PostMapping("/create")
+	public String createProductPost(@ModelAttribute Product product, Model model) {
+		service.create(product);
+		return "redirect:List";
+	}
+	
+	@GetMapping("/list")
+	public String createListPage(Model model) {
+		List<Product> allProducts = service.findAll();
+		model.addAttribute("products",allProducts);
+		return "productList";
+	}
+	
+}
