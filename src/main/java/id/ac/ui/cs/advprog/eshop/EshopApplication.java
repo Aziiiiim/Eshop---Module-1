55932db34cc8772024521f4ddec332333e8fea85
a23barho <azim.barhoumi@imt-atlantique.net>
2025-02-21 19:59:03 +0700

    Fixing code

--- src/main/java/id/ac/ui/cs/advprog/eshop/EshopApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/EshopApplication.java
@@ -4,12 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EshopApplication {
-
-	private EshopApplication() {
-        // Private constructor to prevent instantiation
-    }
-	
+public class EshopApplication {	
 	public static void main(String[] args) {
 		SpringApplication.run(EshopApplication.class, args);
 	}
