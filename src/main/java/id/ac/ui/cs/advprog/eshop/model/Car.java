869fad5cc884fec67e0b53ecb70bd99595c28e2c
a23barho <azim.barhoumi@imt-atlantique.net>
2025-02-24 14:16:06 +0700

    Adding Getters and Setters to Car model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Car.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Car.java
@@ -8,4 +8,36 @@ public class Car {
 	private String carName;
 	private String carColor;
 	private int carQuantity;
+	
+	public String getCarId() {
+		return carId;
+	}
+	
+	public void setCarId(String id) {
+		carId = id;
+	}
+	
+	public String getCarName() {
+		return carName;
+	}
+	
+	public void setCarName(String name) {
+		carName = name;
+	}
+	
+	public String getCarColor() {
+		return carColor;
+	}
+	
+	public void setCarColor(String color) {
+		carColor = color;
+	}
+	
+	public int getCarQuantity() {
+		return carQuantity;
+	}
+
+	public void setCarQuantity(int quantity) {
+		carQuantity= quantity;
+	}
 }
