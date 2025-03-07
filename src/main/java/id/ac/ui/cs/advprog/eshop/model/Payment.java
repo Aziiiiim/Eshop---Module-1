0c348e9227fe53ff35574ad40d77ba727208eb74
a23barho <azim.barhoumi@imt-atlantique.net>
2025-03-07 17:55:47 +0700

    [GREEN] Implement Payment class

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -3,45 +3,72 @@ package id.ac.ui.cs.advprog.eshop.model;
 import java.util.Map;
 
 public class Payment {
-    private String id;
+	private String id;
     private String method;
     private String status;
     private Map<String, String> paymentData;
 
     public Payment(String id, String method, Map<String, String> paymentData, String status) {
+        this.id = id;
+        this.method = method;
+        this.paymentData = paymentData;
+        this.setStatus(status);
     }
 
     public Payment(String id, String method, Map<String, String> paymentData) {
+        this(id, method, paymentData, "PENDING");
     }
 
     public String getId() {
-        return null;
+        return id;
     }
 
     public String getMethod() {
-        return null;
+        return method;
     }
 
     public String getStatus() {
-        return null;
+        return status;
     }
 
     public Map<String, String> getPaymentData() {
-        return null;
+        return paymentData;
     }
 
     public void setStatus(String status) {
-    }
+        if (validateStatus(status)) {
+        	boolean voucherValid = this.getMethod().equals("Voucher") && 
+                    isValidVoucher(this.getPaymentData().get("voucherCode"));
+                    
+        	boolean cashOnDeliveryValid = this.getMethod().equals("CashOnDelivery") && 
+                           isValidCashOnDelivery(this.getPaymentData());
 
+        	if(voucherValid || cashOnDeliveryValid) {
+                this.status = status;
+        	}
+        	else {
+        		this.status = "REJECTED";
+        	}
+        } else {
+            throw new IllegalArgumentException("Invalid payment status: " + status);
+        }
+    }
+    
     private boolean validateStatus(String status) {
-        return false;
+        return status.equals("PENDING") || status.equals("SUCCESS") || status.equals("REJECTED") || status.equals("CANCELLED");
     }
-
+    
     private boolean isValidVoucher(String voucherCode) {
-        return false;
+        return voucherCode != null &&
+               voucherCode.length() == 16 &&
+               voucherCode.startsWith("ESHOP") &&
+               voucherCode.replaceAll("[^0-9]", "").length() == 8;
+    }
+    
+    private boolean isValidCashOnDelivery(Map<String, String> paymentData) {
+        return paymentData.containsKey("address") && paymentData.containsKey("deliveryFee") &&
+               !paymentData.get("address").isEmpty() && !paymentData.get("deliveryFee").isEmpty();
     }
 
-    private boolean isValidCashOnDelivery(Map<String, String> paymentData) {
-        return false;
-    }
+
 }
